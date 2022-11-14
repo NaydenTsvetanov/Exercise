b05f215e5c8fd937962266ffefb5734671724cdc
Naka <n.tsvetanov99@gmail.com>
2022-11-14 19:41:54 +0200

    commit

--- JS-Applications /02. JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
+++ JS-Applications /02. JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
@@ -1,3 +1,11 @@
+const enumIcon = {
+    "Sunny": "&#x2600",
+    "Partly sunny": "&#x26C5",
+    "Overcast": "&#x2601",
+    "Rain": "&#x2614",
+    "Degrees": "&#176"
+}
+
 function attachEvents() {
     console.log("TODO...");
     document.getElementById("submit").addEventListener("click", getWeather);
@@ -23,7 +31,51 @@ async function createForcast(code) {
     const urlUpcoming = `http://localhost:3030/jsonstore/forecaster/upcoming/${code}`;
 
     const responseToday = await fetch(urlToday);
+    const dataToday = await responseToday.json();
+
     const responseUpcoming = await fetch(urlUpcoming);
+    const dataUpcoming = await responseUpcoming.json();
+
+    createToday(dataToday);
+
+}
+
+function createToday(data) {
+    
+    const { condition, high, low } = data.forecast;
+
+    const currentSelection = document.getElementById("current");
+    
+    const conditionContainer = document.createElement("div");
+    conditionContainer.classList.add("forecasts");
+
+    const conditionIconSpan = document.createElement("span");
+    conditionIconSpan.classList.add("condition");
+    conditionIconSpan.textContent = enumIcon[condition];
+
+    const conditionSpan = document.createElement("span");
+    conditionSpan.classList.add("condition");
+
+    const nameSpan = document.createElement("span");
+    nameSpan.classList.add("forecast-data");
+    nameSpan.textContent = data.name;
+
+    const tempSpan = document.createElement("span");
+    tempSpan.classList.add("forecast-data");
+    tempSpan.textContent = `${low}${enumIcon["Degrees"]}/${high}${enumIcon["Degrees"]}`;
+
+    const txtSpan = document.createElement("span");
+    txtSpan.classList.add("forecast-data");
+    txtSpan.textContent = condition;
+
+    conditionSpan.appendChild(nameSpan);
+    conditionSpan.appendChild(tempSpan);
+    conditionSpan.appendChild(txtSpan);
+
+    conditionContainer.appendChild(conditionIconSpan);
+    conditionContainer.appendChild(conditionSpan);
+
+    currentSelection.appendChild(conditionContainer);
 
 }
 
