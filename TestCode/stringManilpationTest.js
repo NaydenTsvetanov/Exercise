443e6b67b29b236d623052768cd609da1664322e
Naka <n.tsvetanov99@gmail.com>
2022-11-28 19:55:33 +0200

    commit

--- TestCode/stringManilpationTest.js
+++ TestCode/stringManilpationTest.js
@@ -1,15 +1,31 @@
-function changeLetters(str) {
+// function changeLetters(str) {
 
-    let arr = str.split("");
+//     let arr = str.split("");
 
-    for(let i = 0; i < arr.length; i++) {
+//     for(let i = 0; i < arr.length; i++) {
 
-        arr[i] = "A";
+//         arr[i] = "A";
 
+//     }
+
+//     return arr.join("");
+
+// }
+
+function solve() {
+
+    let hero = {
+        "Cow" : [100, 100],
+        "Kyr" : [100, 100],
+        "Dog" : [100, 100]
     }
 
-    return arr.join("");
+    let key = "Kyr";
+
+   delete hero[key];
+
+    console.log(`${hero["Dog"]} is Dead`);
 
 }
 
-console.log(changeLetters("I have two doctors, my left leg and my right. When my body and mind are out of gear (and this twin parts of me live at such close quarters that one always catches meloncholy from the other) I know that I have only to call my doctors and I shall be well again. - George Macaulay Trevelyan,  1913"));
+solve();
