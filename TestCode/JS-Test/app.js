96128e1baaa3f2f1472da490fd540580a7ce8bec
Naka <n.tsvetanov99@gmail.com>
2023-01-10 21:33:57 +0200

    commit

--- TestCode/JS-Test/app.js
+++ TestCode/JS-Test/app.js
@@ -1,2 +1,7 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEGHT = canvas.height = 600;
+
+const playerImage = new Image();
