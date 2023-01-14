18a160653a810e1f16224e944c16063ce5722d50
Naka <n.tsvetanov99@gmail.com>
2023-01-14 15:08:49 +0200

    commit

--- TestCode/JS-Test/app.js
+++ TestCode/JS-Test/app.js
@@ -5,4 +5,14 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEGHT = canvas.height = 600;
 
 const playerImage = new Image();
+playerImage.src = "images/character_0008.png";
 
+function animate() {
+
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEGHT);
+    //ctx.fillRect(50, 50, 100, 100);
+    ctx.drawImage(playerImage, 0, 0, 64, 64);
+    requestAnimationFrame(animate);
+
+}
+animate();
